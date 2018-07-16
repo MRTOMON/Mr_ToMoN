f57b48958fca9145bd7d51441df165f720a62a1b
MRTOMON <41242088+MRTOMON@users.noreply.github.com>
2018-07-16 03:53:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,4 +28,35 @@ client.user.setGame(`Nothing`,"http://twitch.tv/S-F")
   console.log('')
 });
 
+const devs = ['id 1' , 'id 2' , 'id 3' , 'id 4'];
+const adminprefix = "البرفكس";
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.sendMessage(`**✅   ${argresult}**`)
+  }
+  });
+
+
+
+
+
+
+
+
 client.login(process.env.BOT_TOKEN);
